f5814b535b1c5f07dc6278b45f79508f064fad14
Pankaj Joshi <Pankaj.Joshi.hp@gmail.com>
2024-12-18 19:36:39 +0530

    branch2

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,16 @@
 import React from "react";
-import useFetch from "./customHooks/useFetch";
-import Addtask from "./components/AddTodo";
+import { UserProvider } from "./customHooks/UserContext";
+import List from "./components/List";
+// import AddTask from "./AddTask";
 
 const App = () => {
-  const [data] = useFetch("https://dummyjson.com/todos?limit=10");
-
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-200 p-4">
-      <Addtask data={data} />
-
-      {/* Main container with a 70% width and centered content */}
-      <div className="w-full h-full  lg:w-[70%] lg:h-auto bg-gray-400 shadow-lg rounded-sm p-6 overflow-y-auto">
-        <ul className="space-y-2">
-          {data &&
-            data.todos.map((todo) => (
-              <li
-                className="text-sm text-gray-700 border-b border-gray-300 pb-1"
-                key={todo.id}
-              >
-                {todo.todo}
-              </li>
-            ))}
-        </ul>
+    <UserProvider>
+      <div>
+        {/* <AddTask /> */}
+        <List />
       </div>
-    </div>
+    </UserProvider>
   );
 };
 
